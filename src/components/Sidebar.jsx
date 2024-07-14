d8d79274d7fa4ad49e9508aeea711354eb7121ed
Adam Evans <adam3van5@gmail.com>
2024-07-14 18:00:36 +0900

    create button component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 export default function Sidebar() {
   function clickHander() {
     console.log('click');
@@ -8,9 +10,7 @@ export default function Sidebar() {
         Your projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-          + Add project
-        </button>
+        <Button>+ Add project</Button>
       </div>
     </aside>
   );
