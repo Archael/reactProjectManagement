7da5ab6aa3490291cb60923d8c98ecaa5ee87ade
Adam Evans <adam3van5@gmail.com>
2024-07-18 19:36:46 +0900

    create new task component

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+
 const NewTask = () => {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(e) {
+    setEnteredTask(e.target.value);
+  }
+
+  function handleClick() {
+    setEnteredTask('');
+  }
+
   return (
     <>
       <div className="flex items-center gap-4">
-        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" type="text" />
-        <button className="text-stone-700 hover:text-stone-900">
+        <input
+          onChange={handleChange}
+          value={enteredTask}
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          type="text"
+        />
+        <button
+          onClick={handleClick}
+          className="text-stone-700 hover:text-stone-900"
+        >
           Add task
         </button>
       </div>
